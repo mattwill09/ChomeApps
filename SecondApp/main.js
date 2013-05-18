05ffebcaee845da5d31153c2e5eb9e47e879e772
Matthew Will <mattwill09@gmail.com>
2013-05-18 15:43:44 -0500

    more realistic todo app

--- SecondApp/main.js
+++ SecondApp/main.js
@@ -6,10 +6,5 @@ chrome.app.runtime.onLaunched.addListener(function() {
         }
     });
     
-    chrome.app.window.create('index.html', {
-        bounds: {
-            width: 500,
-            height: 309
-        }
-    });
+    
 });
