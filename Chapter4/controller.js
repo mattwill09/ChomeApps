0781ff0e0d97239c02de2922d3e029700d8281db
Matthew Will <mattwill09@gmail.com>
2013-05-19 17:25:42 -0500

    added drag drop support

--- Chapter4/controller.js
+++ Chapter4/controller.js
@@ -1,5 +1,61 @@
 function TodoCtrl($scope) {
 
+    var defaultDropText = "Or drop files here";
+    $scope.dropText = defaultDropText;
+    
+    var dragOver = function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var valid = e.dataTransfer && e.dataTransfer.types && 
+            (e.dataTransfer.types.indexOf('Files') >= 0 ||
+             e.dataTransfer.types.indexOf('text/uri-list') >= 0)
+         $scope.$apply(function() {
+            $scope.dropText = valid ? "Drop files and remote images and they will become Todos" :
+                "Can only drop files and remote images here";
+            $scope.dropClass = valid ? "dragging" : "invalid-dragging";
+        });
+    }
+    
+    var dragLeave = function(e) {
+        $scope.$apply(function() {
+            $scope.dropText = defaultDropText;
+            $scope.dropClass = "";
+        });
+    }
+    
+    var drop = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        
+        var newTodos = [];
+        
+        if (e.dataTransfer.types.indexOf('Files') >= 0) {
+            var files = e.dataTransfer.files;
+            for (var i = 0; i < files.length; i++) {
+                var text = files[i].name + ', ' + files[i].size + ' bytes';
+                newTodos.push({text: text, done: false, file: files[i]});
+            }
+        } else {
+            var uri = e.dataTransfer.getData("text/uri-list");
+            newTodos.push({text: uri, done: false, uri: uri});
+        }
+        
+        $scope.$apply(function() {
+            $scope.dropText = defaultDropText;
+            $scope.dropClass = "";
+            
+            for (var i = 0; i < newTodos.length; i++) {
+                $scope.todos.push(newTodos[i]);
+            }
+            
+            $scope.save();
+        });
+    }
+    
+    document.body.addEventListener("dragover", dragOver, false);
+    document.body.addEventListener("dragleave", dragLeave, false);
+    document.body.addEventListener("drop", drop, false);
+    
     chrome.storage.sync.get('todolist', function(value) {
         //the $apply is only necessary to execute the function inside Angular scope
         $scope.$apply(function() {
