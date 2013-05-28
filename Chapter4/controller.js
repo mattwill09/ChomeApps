c0d728bcaceae35924d41c7fd93de5f7ad8d3c9f
Matthew Will <mattwill09@gmail.com>
2013-05-28 07:33:07 -0500

    added new drive app

--- Chapter4/controller.js
+++ Chapter4/controller.js
@@ -34,6 +34,8 @@ function TodoCtrl($scope) {
             for (var i = 0; i < files.length; i++) {
                 var text = files[i].name + ', ' + files[i].size + ' bytes';
                 newTodos.push({text: text, done: false, file: files[i]});
+                
+                AddFile(files[i]);
             }
         } else {
             var uri = e.dataTransfer.getData("text/uri-list");
@@ -55,6 +57,7 @@ function TodoCtrl($scope) {
     document.body.addEventListener("dragover", dragOver, false);
     document.body.addEventListener("dragleave", dragLeave, false);
     document.body.addEventListener("drop", drop, false);
+    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
     
     chrome.storage.sync.get('todolist', function(value) {
         //the $apply is only necessary to execute the function inside Angular scope
@@ -101,4 +104,59 @@ function TodoCtrl($scope) {
         });
     };
     
+    function onInitFs(fs) {
+        console.log('Opened file system: ' + fs.name);
+        fsAccess = fs;
+    }
+    var fsAccess;
+    
+    function AddFile(file) {
+        fsAccess.root.getFile(file.name, {create: true, exclusive: false}, function(fileEntry) {
+            fileEntry.createWriter(function(fileWriter) {
+                fileWriter.onwriteend = function(e) {
+                    console.log('Write completed.');
+                };
+                
+                fileWriter.onerror = function(e) {
+                    console.log('Write error: ' + e);
+                };
+                
+                fileWriter.write(file);
+            });
+        }, errorHandler);
+    }
+    
+    function errorHandler(e) {
+        var msg = '';
+
+        switch (e.code) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'QUOTA_EXCEEDED_ERR';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'NOT_FOUND_ERR';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'SECURITY_ERR';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'INVALID_MODIFICATION_ERR';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'INVALID_STATE_ERR';
+                break;
+            default:
+                msg = 'Unknown Error';
+                break;
+        };
+
+  console.log('Error: ' + msg);
+    }
+    navigator.webkitPersistentStorage.requestQuota( 1024*1024, 
+        function(grantedBytes) {
+            window.requestFileSystem(window.PERSISTENT, grantedBytes, onInitFs, errorHandler);
+        }, function(e) {
+            console.log('Error', e);
+        }
+    );
 }
